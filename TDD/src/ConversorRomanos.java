1e2b2ae31fe0558bcca86f868364b815cf918c98
Edson Borges <edsonn.borgess@gmail.com>
2018-07-27 16:16:29 -0300

    Update ConversorRomanos.java

--- TDD/src/ConversorRomanos.java
+++ TDD/src/ConversorRomanos.java
@@ -10,6 +10,10 @@ public class ConversorRomanos {
         put('I', 1);
         put('V', 5);
         put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
     }};
     
     public int converte(String numeroRomano) {
