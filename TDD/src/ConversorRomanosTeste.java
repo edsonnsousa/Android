f6e6a579fb6869048513234951fa3d7f4f5937c5
Edson Borges <edsonn.borgess@gmail.com>
2018-07-27 17:25:10 -0300

    Update ConversorRomanosTeste.java

--- TDD/src/ConversorRomanosTeste.java
+++ TDD/src/ConversorRomanosTeste.java
@@ -1,35 +1,38 @@
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 public class ConversorRomanosTeste {
 
     @Test
-    public void converteOSimboloI() {
+    public void ConverterUmNumeroRomano() {
        ConversorRomanos conversor = new ConversorRomanos();
        int resultado = conversor.converte("I");
        assertEquals (1, resultado);
     }
     
     @Test
-    public void converteOSimboloV() {
-    	ConversorRomanos conversor = new ConversorRomanos();
-    	int resultado = conversor.converte("V");
-       
-       assertEquals(5, resultado);
-}
-    @Test
-    public void converteOSimboloXX() {
+    public void converterDoisNumeroIguais() {
     	ConversorRomanos conversor = new ConversorRomanos();
     	int resultado = conversor.converte("XX");
        
        assertEquals(20, resultado);
 }
     @Test
-    public void converteOSimboloXI() {
+    public void converterDoisNumerosESegundoNumeroMenor() {
     	ConversorRomanos conversor = new ConversorRomanos();
     	int resultado = conversor.converte("XI");
        
        assertEquals(11, resultado);
+}
+    @Test
+    public void converterDoisNumerosEPrimeiroNumeroMenor() {
+    	ConversorRomanos conversor = new ConversorRomanos();
+    	int resultado = conversor.converte("IX");
+       
+       assertEquals(9, resultado);
+}
+    @Test
+    public void converterTresSimbolosDistintos() {
+    	ConversorRomanos conversor = new ConversorRomanos();
+    	int resultado = conversor.converte("XXVIII");
+       
+       assertEquals(28, resultado);
 }
 }
