b3c503f394688a4196718ea7783d494cebcde145
Edson Borges <edsonn.borgess@gmail.com>
2018-07-27 15:29:25 -0300

    Update ConversorRomanos.java

--- TDD/src/ConversorRomanos.java
+++ TDD/src/ConversorRomanos.java
@@ -17,15 +17,15 @@ public class ConversorRomanos {
         int simboloDaDireita = 0;
 
         for(int i = numeroRomano.length() - 1; i >= 0; i--) {
-            // pega o inteiro referente ao simbolo atual
+            
             int atual = valores.get(numeroRomano.charAt(i));
             
-            // tornar o o numero negativo
-            int multiplicador = 1;
-            if(atual < simboloDaDireita) multiplica = -1;
-            acumula += valores.get(numeroRomano.charAt(i)) * multiplica;
             
-            // atualiza o da direita
+            int multiplicador = 1;
+            if(atual < simboloDaDireita) multiplicador = -1;
+            acumula += valores.get(numeroRomano.charAt(i)) * multiplicador;
+            
+            
             simboloDaDireita = atual;
         }
         return acumula;
